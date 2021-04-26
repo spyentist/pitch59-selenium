0f9d27b940617b0a8d79217f6de29541045ae644
matkat99 <st83440@gmail.com>
2021-04-26 10:07:20 -0600

    stringify body

--- tests/pitch59login.test.js
+++ tests/pitch59login.test.js
@@ -1,39 +1,36 @@
-const fetch = require('node-fetch')
-const config = require('config');
+const fetch = require("node-fetch");
+const config = require("config");
 
-const loginUrl = config.get('pitch59-url') + '/api/account/login';
+const loginUrl = config.get("pitch59-url") + "/api/account/login";
 
 it(`Testing to see if we can signin`, async () => {
-    let options = {
-        method: 'POST',
-        headers: {
-            "content-type":"application/json"
-        },
-        body: {
-            //"emailId": "Shandu@gmail.com",
-            //"password": "Lutendo1"
-            "emailId": "ironman@gmail.com",
-            "password": "BetterThanCap10"
-        }
-    };
+  let options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      //"emailId": "Shandu@gmail.com",
+      //"password": "Lutendo1"
+      emailId: "ironman@gmail.com",
+      password: "BetterThanCap10"
+    })
+  };
 
-    let errorWasCaught = false;
-    let errorCaught = null;
-    let json=null;
-    try {
-        const response = await fetch(loginUrl, options);
-        json = await response.json();
-        console.log('Response',json);
-        
-    } catch (exception) {
-        errorCaught = exception;
-        errorWasCaught = true;
-        throw console.log(errorCaught);
-    }
+  let errorWasCaught = false;
+  let errorCaught = null;
+  let json = null;
+  try {
+    const response = await fetch(loginUrl, options);
+    json = await response.json();
+    console.log("Response", json);
+  } catch (exception) {
+    errorCaught = exception;
+    errorWasCaught = true;
+    throw console.log(errorCaught);
+  }
 
-    expect(errorWasCaught).toBe(false);
-    console.log(json.code)
-    expect(json.code).toBe(2012);
+  expect(errorWasCaught).toBe(false);
+  console.log(json.code);
+  expect(json.code).toBe(2012);
 });
-
-
