aaef10ed9aaf78bfe59523de4f74f5e2b2943aec
Nathan Martherus <nathanmartherus@Nathans-MacBook-Pro-2.local>
2021-05-13 10:32:21 -0600

    fixed loginlogout.test by making status a global variable

--- tests/pitch59logout.test.js
+++ tests/pitch59logout.test.js
@@ -10,8 +10,7 @@ it(`Testing to see if we can signout`, async () => {
         method: 'POST',
         uri: signoutUrl,
         headers: {
-//            authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTSUQiOiJYVVBESlRHZ04va2tXM3VLYlNTSGJYUTRJWGV5Z09BUjlyYUVJMTJSdHB5L1FxNmdNRFQzK3lhM0xic29ETVI3IiwiVUlEIjoiUlFSQlVaSDJjMWhEZ0lSWXYzYk5yU2w2RVRxY0I3bVZnL0xEckppUmMwRzVyMmlCUGhKT3JVU3Fxd0laeFFraiIsIlJJRCI6IkZOeEVLL0NlMHgyT1JxU1p6dEZRbGJ3ZERzZ1pnWUFpZWJQdDNYbkQ4cTQ9Iiwicm9sZSI6IlVzZXIiLCJuYmYiOjE2MjA0MzE2NDksImV4cCI6MTYyMDc5MTY0OSwiaWF0IjoxNjIwNDMxNjQ5LCJpc3MiOiJQaXRjaDU5QVBJIiwiYXVkIjoiUGl0Y2g1OVdlYkNsaWVudCJ9.tmxjq11y-HjXRo4yIFAEXg32MqPS3qO3C2Dg_j29VBU"
-            'Authorization' : 'bearer' + token
+            'Authorization' : "bearer" + token
         }
     };
 
