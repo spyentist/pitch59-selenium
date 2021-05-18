5d046867a7fd9156c7ea46206d82efb412a0d0dd
Craig J Lindstrom <craig@esage.com>
2021-05-18 11:34:02 -0600

    works in postman :( but not here

--- tests/Pitch59addBusiness.test.js
+++ tests/Pitch59addBusiness.test.js
@@ -9,11 +9,12 @@ it(`addBusiness test`, async () => {
     uri: addBusinessUrl,
     headers: {
       "content-type": "application/json",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTSUQiOiI1eXRCTVc3Qm5BRTdQQVNKT3ZvblRmUzBmY09QNGsxb0RzQW1jN0xIeEVDc2hMekxYZ3NUYktEckUzMWFUTGdkIiwiVUlEIjoib0QrVit5ZEVYL3NpRzRtUWZUUzN6a2JPMzRCU3lDNTU2OU5SUkhra0w4UTc4eUxSL0hHdWJNT2g0MWNBdGZNaSIsIlJJRCI6IjEweWQ0Sys4T2Y5UFViT1o4ZDhaOUNhQVVGMWxwSW9hdGFCRXp1TWh3WTg9Iiwicm9sZSI6IlVzZXIiLCJuYmYiOjE2MjEyNjEyODUsImV4cCI6MTYyMTYyMTI4NSwiaWF0IjoxNjIxMjYxMjg1LCJpc3MiOiJQaXRjaDU5QVBJIiwiYXVkIjoiUGl0Y2g1OVdlYkNsaWVudCJ9.9ZHIZ1AwPKNuS8rQ_T0EClAEC4m8iC2cQj6AdqRPhZs"
+      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTSUQiOiI1eXRCTVc3Qm5BRTdQQVNKT3ZvblRmUzBmY09QNGsxb0RzQW1jN0xIeEVDc2hMekxYZ3NUYktEckUzMWFUTGdkIiwiVUlEIjoib0QrVit5ZEVYL3NpRzRtUWZUUzN6a2JPMzRCU3lDNTU2OU5SUkhra0w4UTc4eUxSL0hHdWJNT2g0MWNBdGZNaSIsIlJJRCI6IjEweWQ0Sys4T2Y5UFViT1o4ZDhaOUNhQVVGMWxwSW9hdGFCRXp1TWh3WTg9Iiwicm9sZSI6IlVzZXIiLCJuYmYiOjE2MjEyNjEyODUsImV4cCI6MTYyMTYyMTI4NSwiaWF0IjoxNjIxMjYxMjg1LCJpc3MiOiJQaXRjaDU5QVBJIiwiYXVkIjoiUGl0Y2g1OVdlYkNsaWVudCJ9.9ZHIZ1AwPKNuS8rQ_T0EClAEC4m8iC2cQj6AdqRPhZs",
+      "accept": "application/json"
     },
     body: JSON.stringify({
       "businessType": "resume",
-      "businessName": "Craig2" //must be a unique business
+      "businessName": "Craig"
     })
   };
 
@@ -31,6 +32,7 @@ it(`addBusiness test`, async () => {
   }
 
   expect(errorWasCaught).toBe(false);
-  console.log(json.code);
+  console.log(json);
+  expect(json.code).toBe(8000)
 
 });
