f1082229590c5444098bc99019517f7453f8d157
hotfudgebrownlee <jarenbrownlee@gmail.com>
2021-05-18 09:24:16 -0600

    update user test

--- tests/pitch59logout.test.js
+++ tests/pitch59logout.test.js
@@ -1,15 +1,52 @@
-const fetch = require('node-fetch')
-const config = require('config');
+const fetch = require("node-fetch");
+const config = require("config");
+let log_token = null;
+const loginUrl = config.get("pitch59-url") + "/api/account/login";
 
-const signoutUrl = config.get('pitch59-url') + '/dev/api/account/logout';
+it(`Testing to see if we can signin`, async () => {
+  let options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify({
+      //"emailId": "Shandu@gmail.com",
+      //"password": "Lutendo1"
+      emailId: "ironman@gmail.com",
+      password: "BetterThanCap10"
+    })
+  };
+
+  let errorWasCaught = false;
+  let errorCaught = null;
+  let json = null;
+  try {
+    const response = await fetch(loginUrl, options);
+    json = await response.json();
+
+    console.log("Response", json);
+    log_token = json.data.token;
+    console.log("Token", log_token);
+
+  } catch (exception) {
+    errorCaught = exception;
+    errorWasCaught = true;
+    throw console.log(errorCaught);
+  }
+
+  expect(errorWasCaught).toBe(false);
+  console.log(json.code);
+  expect(json.code).toBe(2012);
+});
+// console.log('Bearer', log_token);
+const signoutUrl = config.get('pitch59-url') + '/api/account/logout';
 
 it(`Testing to see if we can signout`, async () => {
     const date = new Date();
     let options = {
         method: 'POST',
-        uri: signoutUrl,
         headers: {
-            authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTSUQiOiJ2WUNiU1NyS2JaUWMrYkNPcXZFb0NvMzJpbVVoWFdhMTF6NEJZK2oxZkZHWWJhbTdZb0RMYnFKRFVOQ2ZqL1NFIiwiVUlEIjoiaWlsd3BEWHYzWDlRZis3MGpXbzUreDFOdGI5eWlMbWNjYjk1NGdmNWgrZlBXNXdGV2F3VDBpVDRrR2Jid1lUVSIsIlJJRCI6IlFUbUovM0xFaEM1NU9FQVRIRUExSDRxQ29rY0kyczdseEUzSW5QTzdMcU09Iiwicm9sZSI6IlVzZXIiLCJuYmYiOjE2MjAyMjIzNzcsImV4cCI6MTYyMDU4MjM3NywiaWF0IjoxNjIwMjIyMzc3LCJpc3MiOiJQaXRjaDU5QVBJIiwiYXVkIjoiUGl0Y2g1OVdlYkNsaWVudCJ9.26mI78XtlZYRPF0Wa-GjqxRPzSSDey7qiympllhlip0"
+            'Authorization': `bearer ${log_token}`
         }
     };
 
@@ -18,9 +55,10 @@ it(`Testing to see if we can signout`, async () => {
 
     try {
         const response = await fetch(signoutUrl, options);
-        const json = await response.json();
-        console.log('Response',json);
-        return response;
+        const txt = await response.text();
+        console.log('Response',txt);
+        const status = response.status;
+        console.log('Status',status);
         
     } catch (exception) {
         errorCaught = exception;
@@ -29,6 +67,5 @@ it(`Testing to see if we can signout`, async () => {
     }
 
     expect(errorWasCaught).toBe(false);
+    expect(status).toBe(200);
 });
-
-
