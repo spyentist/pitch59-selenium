872f50757b03eb5d16f86b25689534cee8a24772
Craig J Lindstrom <craig@esage.com>
2021-05-20 12:10:24 -0600

    added notes

--- tests/Pitch59addBusiness.test.js
+++ tests/Pitch59addBusiness.test.js
@@ -1,7 +1,9 @@
+//TODO add create user (to avoid token dependency) then add buisness then remove business and user
 const fetch = require("node-fetch");
 const config = require("config");
 
 const addBusinessUrl = config.get("pitch59-url") + "/api/business/add-business";
+
 const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJTSUQiOiI1eXRCTVc3Qm5BRTdQQVNKT3ZvblRmUzBmY09QNGsxb0RzQW1jN0xIeEVDc2hMekxYZ3NUYktEckUzMWFUTGdkIiwiVUlEIjoib0QrVit5ZEVYL3NpRzRtUWZUUzN6a2JPMzRCU3lDNTU2OU5SUkhra0w4UTc4eUxSL0hHdWJNT2g0MWNBdGZNaSIsIlJJRCI6IjEweWQ0Sys4T2Y5UFViT1o4ZDhaOUNhQVVGMWxwSW9hdGFCRXp1TWh3WTg9Iiwicm9sZSI6IlVzZXIiLCJuYmYiOjE2MjEyNjEyODUsImV4cCI6MTYyMTYyMTI4NSwiaWF0IjoxNjIxMjYxMjg1LCJpc3MiOiJQaXRjaDU5QVBJIiwiYXVkIjoiUGl0Y2g1OVdlYkNsaWVudCJ9.9ZHIZ1AwPKNuS8rQ_T0EClAEC4m8iC2cQj6AdqRPhZs"
 
 it(`addBusiness test`, async () => {
@@ -9,13 +11,13 @@ it(`addBusiness test`, async () => {
     method: "POST",
     uri: addBusinessUrl,
     headers: {
-      "authorization": "bearer " + token ,
+      "authorization": "bearer " + token , //business will be created for account represented by the bearer token
       "accept": "*",
       "content-type":"application/json"
     },
     body: JSON.stringify({
       "businessType": "resume",
-      "businessName": "Craig"
+      "businessName": "Craig" //api does note validate unique businessName
     })
   };
 
