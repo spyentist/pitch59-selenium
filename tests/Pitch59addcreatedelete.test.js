a26b99e20ce9e65c336f69bbafccf7a6056aa5f7
ShaneWitbeck <77181662+ShaneWitbeck@users.noreply.github.com>
2021-05-25 10:01:45 -0700

    Update Pitch59addcreatedelete.test.js

--- tests/Pitch59addcreatedelete.test.js
+++ tests/Pitch59addcreatedelete.test.js
@@ -31,7 +31,7 @@ it(`Testing to see if we can signin`, async () => {
   expect(errorWasCaught).toBe(false);
   console.log(json.code);
   expect(json.code).toBe(2012);
-
+})
 //TODO add create user (to avoid token dependency) then add buisness then remove business and user
 const fetch = require("node-fetch");
 const config = require("config");
