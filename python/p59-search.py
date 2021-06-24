ff89b1471b9aa0e31be0f90752a0e4ed3c3bf3d9
Jacob Perry <thefloatingtree@gmail.com>
2021-06-24 16:00:41 -0600

    Incremental

--- python/p59-search.py
+++ python/p59-search.py
@@ -18,17 +18,17 @@ def main(argv):
 
    if headless:
       driver = webdriver.Remote("http://127.0.0.1:4444/wd/hub", DesiredCapabilities.CHROME, options=options) 
-   else:
+   else: 
       driver = webdriver.Chrome(options=options)
    return driver
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
 options = webdriver.ChromeOptions()
-options.headless = False
 options.add_argument(f'user-agent={user_agent}')
 options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--no-sandbox')
+options.add_argument('--headless')
 
 driver = main(sys.argv[1:])
 driver.get("https://public.p59.dev/welcome")
